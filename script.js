4b91fd46d75e07a653bb9b62d5fa5fdc35160351
TreKey <trekey44@gmail.com>
2020-11-24 11:25:37 -0500

    files added

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let btnAdd = document.querySelector('#add');
+let btnSubtract = document.querySelector('#subtract');
+let input = document.querySelector('input');
+
+btnAdd.addEventListener('click', () =>{
+  input.value = parseInt(input.value) + 1;
+});
+
+btnSubtract.addEventListener('click', () =>{
+  input.value = parseInt(input.value) - 1;
+});
+
